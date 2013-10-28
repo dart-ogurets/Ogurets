66d4dcd63adc4ead4a56581e833484a072cb4d0c
Danil Kornishev <dkornishev@amplify.com>
2013-10-28 16:47:38 -0400

    basic parsing work

--- example/gherkin/test_feature.feature
+++ example/gherkin/test_feature.feature
@@ -1,7 +1,13 @@
-Feature: Making sure that parser works
+@Ftag
+@Ftag2 @Ftag3 @Ftag4
+@feature @feature2 @feature3 Feature: Feature Making sure that parser works
 
-  Scenario: Parser is working
-	Given parser is working
-	When I run dherkin
-	And the phase of the moon is "favorable"
-	Then everything "works"
+  @STAG1
+  @STAG2
+  @STAG3 @STAG4 Scenario: Scenario Parser is working
+    Given parser is working
+    When I run dherkin
+    And  the phase of the moon is "favorable"
+    Then everything "works"
+    But work still goes on
+
--- example/run.dart
+++ example/run.dart
@@ -1,15 +1,86 @@
-import "dart:mirrors";
 import "dart:io";
-import "package:dherkin/dherkin.dart";
+import "dart:async";
+import "dart:mirrors";
+import "../lib/dherkin.dart";
 
+
+RegExp tagsPattern = new RegExp(r"(@[^@\r\n\t ]+)");
+RegExp featurePattern = new RegExp(r"Feature\s*:\s*(.+)");
+RegExp scenarioPattern = new RegExp(r"Scenario\s*:\s*(.+)");
 
 void main() {
-  //  Parse
-  //  Locate stepdefs
-  //  Output verbiage for missing stepdefs
-}
+  File file = new File("example/gherkin/test_feature.feature");
 
-@StepDef("Aloha")
-step1() {
-  
+  var feature;
+  var currentScenario;
+  file.readAsLines().then((List<String> contents) {
+    var tags = [];
+
+    var lineIter = contents.iterator;
+
+    while (lineIter.moveNext()) {
+      var line = lineIter.current;
+
+      var iter = tagsPattern.allMatches(line).iterator;
+      while (iter.moveNext()) {
+        var match = iter.current;
+        print(match.group(1));
+        tags.add(match.group(1));
+      }
+
+      iter = featurePattern.allMatches(line).iterator;
+      while (iter.moveNext()) {
+        var match = iter.current;
+        print(match.group(1));
+        feature = new Feature(match.group(1));
+        feature.tags = tags;
+        tags = [];
+      }
+
+      iter = scenarioPattern.allMatches(line).iterator;
+      while (iter.moveNext()) {
+        var match = iter.current;
+        print(match.group(1));
+        currentScenario = new Scenario(match.group(1));
+        currentScenario.tags = tags;
+        feature.scenarios.add(currentScenario);
+        tags = [];
+      }
+
+    }
+
+  }).whenComplete(() {
+    print(feature);
+  });
+
+//  Locate stepdefs
+//  Output verbiage for missing stepdefs
+  }
+  @StepDef("Aloha")
+  step1() {
+
+  }
+
+  class Feature {
+    String name;
+    List<String> tags;
+    List<Scenario> scenarios = [];
+
+    Feature(this.name);
+
+    String toString() {
+      return "$name $tags\n $scenarios";
+    }
+  }
+
+class Scenario {
+  String name;
+  List<String> tags;
+
+  Scenario(this.name);
+
+  String toString() {
+    return "$name $tags";
+  }
+
 }
