c23a1b617ce8981406e3edc97045ae8265a58ca1
Goutte <antoine.goutenoir@gmail.com>
2014-06-22 17:58:02 +0000

    More consistent runner output

--- lib/dherkin.dart
+++ lib/dherkin.dart
@@ -64,12 +64,12 @@ Future run(args) {
       return c.future;
     }).whenComplete(() => Future.wait(featureFutures).whenComplete((){
       // Tally the failed / passed features
-      _buffer.writeln("-------------------");
+      _buffer.writeln("------------------");
       if (runStatus.passedFeaturesCount > 0) {
-        _buffer.writeln("Passed features : ${runStatus.passedFeaturesCount}", color: "green");
+        _buffer.writeln("Features passed: ${runStatus.passedFeaturesCount}", color: "green");
       }
       if (runStatus.failedFeaturesCount > 0) {
-        _buffer.writeln("Failed features : ${runStatus.failedFeaturesCount}", color: "red");
+        _buffer.writeln("Features failed: ${runStatus.failedFeaturesCount}", color: "red");
       }
       _buffer.flush();
       // Tally the missing stepdefs boilerplate
