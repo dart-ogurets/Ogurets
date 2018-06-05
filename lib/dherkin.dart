ac23306a02b3933153498c4f40ccd629be2de2fd
PROL-LAP-DOS\Sacha <sacha.dolski@prolutions.ch>
2018-06-05 11:01:42 +0200

    Using async / await everywhere a Future was used

--- lib/dherkin.dart
+++ lib/dherkin.dart
@@ -5,7 +5,6 @@ import "dart:async";
 
 import 'package:args/args.dart';
 import "package:log4dart/log4dart.dart";
-import "package:worker/worker.dart";
 
 import 'dherkin_core.dart';
 export 'dherkin_core.dart';
@@ -20,8 +19,7 @@ ResultBuffer _buffer = new ConsoleBuffer(); // TODO instantiate based on args
  *
  * We should continue on the OO design pattern and make a DherkinRunner or something.
  */
-Future run(args) {
-  var sw = new Stopwatch()..start();
+run(args) async {
   var options = _parseArguments(args);
 
   LoggerFactory.config[".*"].debugEnabled = options["debug"];
@@ -34,58 +32,34 @@ Future run(args) {
     runTags = options["tags"].split(",");
   }
 
-  var worker = new Worker(spawnLazily: false, poolSize: Platform.numberOfProcessors);
-
   var featureFiles = options.rest;
-
   RunStatus runStatus = new RunStatus();
 
-  var featureFutures = [];
-  return findStepRunners().then((stepRunners) {
-    Completer allDone = new Completer();
-    Future.forEach(featureFiles, (filePath) {
-      Completer c = new Completer();
-      new File(filePath).readAsLines().then((List<String> contents) {
-        worker.handle(new GherkinParserTask(contents, filePath)).then((feature) {
-          Future f = feature.execute(stepRunners, runTags: runTags, worker: worker, debug: options["debug"]);
-          f.then((FeatureStatus featureStatus){
-            if (featureStatus.failed) {
-              runStatus.failedFeatures.add(featureStatus);
-            } else {
-              runStatus.passedFeatures.add(featureStatus);
-            }
-            _buffer.merge(featureStatus.buffer);
-            _buffer.flush();
-          });
-          featureFutures.add(f);
-          c.complete();
-        });
-      });
-      return c.future;
-    }).whenComplete(() => Future.wait(featureFutures).whenComplete((){
-      // Tally the failed / passed features
-      _buffer.writeln("==================");
-      if (runStatus.passedFeaturesCount > 0) {
-        _buffer.writeln("Features passed: ${runStatus.passedFeaturesCount}", color: "green");
-      }
-      if (runStatus.failedFeaturesCount > 0) {
-        _buffer.writeln("Features failed: ${runStatus.failedFeaturesCount}", color: "red");
-      }
-      _buffer.flush();
-      // Tally the missing stepdefs boilerplate
-      _buffer.write(runStatus.boilerplate, color: "yellow");
-      _buffer.write("----------------------------------------------------");
-      _buffer.write("\nExecuted ${runStatus.passedFeaturesCount + runStatus.failedFeaturesCount} features in ${sw.elapsed.inSeconds} seconds\n", color: 'white');
-      _buffer.write("----------------------------------------------------");
-      _buffer.flush();
-      // Close the runner
-      worker.close();
-      allDone.complete(runStatus);
-    }));
-
-    return allDone.future;
-  });
-
+  var stepRunners = await findStepRunners();
+  for (String filePath in featureFiles) {
+    List<String> contents = await new File(filePath).readAsLines();
+    Feature feature = await new GherkinParserTask(contents, filePath).execute();
+    FeatureStatus featureStatus = await feature.execute(stepRunners, runTags: runTags, debug: options["debug"]);
+    if (featureStatus.failed) {
+      runStatus.failedFeatures.add(featureStatus);
+    } else {
+      runStatus.passedFeatures.add(featureStatus);
+    }
+    _buffer.merge(featureStatus.buffer);
+    _buffer.flush();
+  }
+  // Tally the failed / passed features
+  _buffer.writeln("==================");
+  if (runStatus.passedFeaturesCount > 0) {
+    _buffer.writeln("Features passed: ${runStatus.passedFeaturesCount}", color: "green");
+  }
+  if (runStatus.failedFeaturesCount > 0) {
+    _buffer.writeln("Features failed: ${runStatus.failedFeaturesCount}", color: "red");
+  }
+  _buffer.flush();
+  // Tally the missing stepdefs boilerplate
+  _buffer.write(runStatus.boilerplate, color: "yellow");
+  _buffer.flush();
 }
 
 /**
@@ -98,4 +72,3 @@ ArgResults _parseArguments(args) {
   argParser.addOption("tags");
   return argParser.parse(args);
 }
-
